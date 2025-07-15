72edf00378cfe751d988f4e6df51fe9fcf7f88bd
fady emad <fadyemad933@gmail.com>
2025-07-15 05:55:02 +0300

    v1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-unused-vars": "off"
+    },
+  },
 ];
 
 export default eslintConfig;
